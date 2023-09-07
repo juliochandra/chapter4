9fbe2b86a88d74f54be36a0ab7985ca36873965b
juliochandra <juliochandra@gmail.com>
2023-09-07 16:13:29 +0700

    update task loop

--- loop.js
+++ loop.js
@@ -12,31 +12,81 @@
 //   console.log(jumlah);
 // }
 
+let n = 5;
+
 console.log("");
 console.log("===Pola Segitiga Naik===");
 console.log("");
 
-let n = 5;
-let star = "";
-
+let row = "";
 for (let i = 1; i <= n; i++) {
-  for (let j = 1; j <= i; j++) {
-    star += "*";
+  row = "";
+  for (let j = 0; j < i; j++) {
+    row += "*";
   }
-  star += "\n";
+  console.log(row);
 }
-console.log(star);
 
 console.log("");
 console.log("===Pola Segitiga Terbalik===");
 console.log("");
 
-let star2 = "";
-
 for (let i = n; i >= 1; i--) {
-  for (let j = 1; j <= i; j++) {
-    star2 += "*";
+  row = "";
+  for (let j = 0; j < i; j++) {
+    row += "*";
   }
-  star2 += "\n";
+  console.log(row);
 }
-console.log(star2);
+
+console.log("");
+console.log("===Pola Segitiga Berlian===");
+console.log("");
+
+// Loop untuk menciptakan segitiga atas dari pola berlian
+let space = "";
+let star = "";
+let o = 4;
+
+for (let i = 1; i < 2 * n; i++) {
+  // console.log(i);
+  if (i <= n) {
+    space = "";
+    star = "";
+    for (let j = 1; j <= n - i; j++) {
+      space += " ";
+    }
+    for (let k = 1; k <= i * 2 - 1; k++) {
+      star += "*";
+    }
+    console.log(space + star);
+  }
+  if (i > n) {
+    space = "";
+    star = "";
+    for (let j = 1; j <= i - n; j++) {
+      space += " ";
+    }
+    for (let k = 1; k <= o * 2 - 1; k++) {
+      star += "*";
+    }
+    o--;
+    console.log(space + star);
+  }
+}
+
+console.log("");
+console.log("===Pola Segitiga Bilangan===");
+console.log("");
+
+let angka = "";
+for (let i = 1; i <= n; i++) {
+  angka = "";
+  for (let j = 1; j <= i; j++) {
+    angka += j;
+    if (j < i) {
+      angka += "*";
+    }
+  }
+  console.log(angka);
+}
